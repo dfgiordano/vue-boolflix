b60d7f8df141a35b5e77cdaf5bb3be9136bd6a69
dfgiordano <federico.giordano90@gmail.com>
2021-06-14 12:37:19 +0200

    inizio esercizio

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <img src="../assets/boolfix.png" alt="boolfix_logo">
+        <div>
+             <input type="text" placeholder="Cerca...">
+            <button>
+
+            </button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style lang="scss">
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 30px;
+        height: 100px;
+        background-color: yellow;
+    }
+    button {
+        height: 20px;
+        width: 50px;
+    }
+</style>
---
+++ src/components/ListaFilm.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        
+    </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "Listafilm",
+    created: function () {
+        axios
+            .get('https://api.themoviedb.org/3/search/movie', {
+                params : {
+                    api_key: "b42cd1206725e3a8b0314f1c43fd3e61",
+                    query: "pirati",
+                    language: "it-IT",
+                }
+            })
+            .then(
+                (res) => {
+                    console.log(res);
+                }
+            );
+    }
+}    
+</script>
+
+<style lang="scss">
+
+</style>
